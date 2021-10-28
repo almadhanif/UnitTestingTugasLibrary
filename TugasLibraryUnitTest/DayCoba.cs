4465ba0aa989cd9743051fbada545bdda839343d
almadhanif <75262546+almadhanif@users.noreply.github.com>
2021-10-28 12:41:51 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/DayCoba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class DayCoba
+    {
+        private Day hari;
+        [SetUp]
+        public void Init() {
+            hari = new Day();
+        }
+        [Test]
+        public void DayTest() 
+        {
+            Assert.AreEqual("Senin", hari.NameOfDay(1));
+            Assert.AreEqual("Selasa", hari.NameOfDay(2));
+            Assert.AreEqual("Rabu", hari.NameOfDay(3));
+            Assert.AreEqual("Kamis", hari.NameOfDay(4));
+            Assert.AreEqual("Jum'at", hari.NameOfDay(5));
+            Assert.AreEqual("Sabtu", hari.NameOfDay(6));
+            Assert.AreEqual("Minggu", hari.NameOfDay(7));
+
+        }
+    }
+}
---
+++ TugasLibraryUnitTest/UserCoba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class UserCoba
+    {
+        private User user;
+        [SetUp]
+        public void Init() {
+            user = new User();
+        }
+
+        [Test]
+
+        public void IsvalidUserTest()
+        {
+
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+            Assert.IsFalse(user.IsValidUser("firman", "hehe"));
+        }
+        
+    }
+}
